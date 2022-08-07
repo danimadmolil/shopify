8a0e5861597bb1ba374fcfe0007365f01656065f
danimadmolil <dani.madmolil@gmail.com>
2022-08-07 18:50:55 +0430

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Title } from "@mui/icons-material";
+import { AppBar, Button } from "@mui/material";
+import { supabase } from "./services/auth/supbaseAuth";
+import "./App.css";
+import Header from "./parts/Header";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { useState } from "react";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
