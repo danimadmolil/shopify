a8d013cd7b7d32df99fa509d6dcbd8d72fe0ad3e
danimadmolil <dani.madmolil@gmail.com>
2022-08-13 01:04:27 +0430

    clean prisma files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { Title } from "@mui/icons-material";
 import { AppBar, Button } from "@mui/material";
-import { supabase } from "./services/auth/supbaseAuth";
+
 import "./App.css";
 import Header from "./parts/Header";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
+
+import { Provider } from "react-redux";
+
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header></Header>
+        <Routes>
+          {/* <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
